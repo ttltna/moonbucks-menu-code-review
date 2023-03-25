d0f663b938193c1cfe04d54e7597f924cb84d91f
ttltna <ttltnaaa@gmail.com>
2023-03-26 02:00:37 +0900

    function을 arrowfunction으로 바꾸고 innerHTML과 innerText를 textContent로 바꿈 menuSubmit에서 1개이상의 역활을 하는데 바꿔야함

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,14 @@ const menuInput = document.getElementById("espresso-menu-name");
 const menuSubmitButton = document.getElementById("espresso-menu-submit-button");
 const menuForm = document.getElementById("espresso-menu-form");
 const menuList = document.getElementById("espresso-menu-list");
+const menuListCount = document.querySelector(".menu-count");
 const pattern = /[a-zA-Z가-힣]{1,}[\s]*$/;
 const modifyClassName =
   "bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button";
 const removeClassName = "bg-gray-50 text-gray-500 text-sm menu-remove-button";
 
 /*
- *** innerText 보단 textContent를 사용할 것
+ *** innerText는 reflow가 발생하므로 textContent를 사용할 것
  *** innerHTML은 보안에 취약하다.
  */
 
@@ -42,19 +43,39 @@ const checkPattern = (menu) => {
   }
 };
 
-const handleModifyBtnClick = (contents) => {};
+const countingMenuList = () => {
+  menuListCount.textContent = `총 ${menuList.childElementCount}개`;
+};
 
-menuForm.addEventListener("submit", function BlockSubmit(event) {
-  event.preventDefault();
-});
-
-menuSubmitButton.addEventListener("click", function menuSubmit() {
+const menuSubmit = () => {
   if (checkPattern(menuInput.value)) {
     const liTag = creatLiTag();
     const spanTag = createSpanTag(menuInput.value);
-    const revicebtn = createBtnTag(modifyClassName, "수정");
-    const delbtn = createBtnTag(removeClassName, "삭제");
-    liTag.append(spanTag, revicebtn, delbtn);
+    const modifyBtn = createBtnTag(modifyClassName, "수정");
+    const removeBtn = createBtnTag(removeClassName, "삭제");
+    liTag.append(spanTag, modifyBtn, removeBtn);
     menuList.append(liTag);
+
+    /* 어떻게 해야 외부에서 spanTag.textContent에 prompt값을 줄 수 있는지 */
+    modifyBtn.addEventListener("click", () => {
+      spanTag.textContent = prompt("수정하실 이름을 적어주세요");
+    });
+
+    /* 어떻게 해야 외부에서 liTag.remove()를 할 수 있는지 */
+    removeBtn.addEventListener("click", () => {
+      if (confirm("삭제하시겠습니까?")) {
+        liTag.remove();
+        countingMenuList();
+      }
+    });
+    InitializationInput();
+    countingMenuList();
   }
+};
+
+menuForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  menuSubmit();
 });
+
+menuSubmitButton.addEventListener("click", menuSubmit);
