71c1335b20899490a5c63a4bc6356b232fe8ee6d
ttltna <ttltnaaa@gmail.com>
2023-03-28 01:14:55 +0900

    start Step2 localstorage

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+//Step1
 const menuInput = document.getElementById("espresso-menu-name");
 const menuSubmitButton = document.getElementById("espresso-menu-submit-button");
 const menuForm = document.getElementById("espresso-menu-form");
@@ -7,6 +8,23 @@ const pattern = /[a-zA-Z가-힣]{1,}[\s]*$/;
 const modifyClassName =
   "bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button";
 const removeClassName = "bg-gray-50 text-gray-500 text-sm menu-remove-button";
+//*Step1
+
+//Step2
+const moonbucksMenu = document.querySelector(".flex-wrap");
+const h2Tag = document.querySelector(".mt-1");
+const inputLabel = document.querySelector(".input-label");
+const mMA = [
+  //moonbucksMenuArray
+  ["espresso", "☕ 에스프레소 메뉴 관리", "에스프레소 메뉴 이름"],
+  ["frappuccino", "🥤 프라푸치노 메뉴 관리", "프라푸치노 메뉴 이름"],
+  ["blended", "🍹 블렌디드 메뉴 관리", "블렌디드 메뉴 이름"],
+  ["teavana", "🫖 티바나 메뉴 관리", "티바나 메뉴 이름"],
+  ["desert", "🍰 디저트 메뉴 관리", "디저트 메뉴 이름"],
+];
+const nowCategory = "espresso";
+const localContents = new Object();
+//*Step2
 
 /*
  *** innerText는 reflow가 발생하므로 textContent를 사용할 것
@@ -68,8 +86,68 @@ const menuSubmit = () => {
         countingMenuList();
       }
     });
+    const localObj = {
+      category: nowCategory,
+      value: menuInput.value,
+    };
+    localContents.name = [...menuInput.value];
+    localContents.stockArticles = 10;
+    window.localStorage.setItem(nowCategory, JSON.stringify(localContents));
     InitializationInput();
     countingMenuList();
+  }
+};
+
+const espressoMenu = () => {
+  h2Tag.textContent = mMA[0][1];
+  inputLabel.textContent = mMA[0][2];
+  menuInput.placeholder = mMA[0][2];
+  nowCategory = "espresso";
+};
+const frappuccinoMenu = () => {
+  h2Tag.textContent = mMA[1][1];
+  inputLabel.textContent = mMA[1][2];
+  menuInput.placeholder = mMA[1][2];
+  nowCategory = "frappuccino";
+};
+const blendedMenu = () => {
+  h2Tag.textContent = mMA[2][1];
+  inputLabel.textContent = mMA[2][2];
+  menuInput.placeholder = mMA[2][2];
+  nowCategory = "blended";
+};
+const teavanaMenu = () => {
+  h2Tag.textContent = mMA[3][1];
+  inputLabel.textContent = mMA[3][2];
+  menuInput.placeholder = mMA[3][2];
+  nowCategory = "teavana";
+};
+const desertMenu = () => {
+  h2Tag.textContent = mMA[4][1];
+  inputLabel.textContent = mMA[4][2];
+  menuInput.placeholder = mMA[4][2];
+  nowCategory = "desert";
+};
+
+const checkCategory = (category) => {
+  for (let i = 0; i < 5; i++) {
+    if (category === mMA[i][0]) {
+      if (i === 0) {
+        espressoMenu();
+      }
+      if (i === 1) {
+        frappuccinoMenu();
+      }
+      if (i === 2) {
+        blendedMenu();
+      }
+      if (i === 3) {
+        teavanaMenu();
+      }
+      if (i === 4) {
+        desertMenu();
+      }
+    }
   }
 };
 
@@ -79,3 +157,8 @@ menuForm.addEventListener("submit", (event) => {
 });
 
 menuSubmitButton.addEventListener("click", menuSubmit);
+
+moonbucksMenu.addEventListener("click", (event) => {
+  // console.dir(event.target.dataset);
+  checkCategory(event.target.dataset.categoryName);
+});
